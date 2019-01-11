18a1f0c7b6af5752f75fb138bcdc9cb929cfa525
vinod v <vinodv@sparksupport.com>
2019-01-11 09:41:05 +0530

    completed the circle and toggle

--- d3charts.js
+++ d3charts.js
@@ -1,18 +1,21 @@
-      var width = 500;
-      var height = 500;
-      var margin = {top:2,left:20,right:2,bottom:20};
+      var width = 600;
+      var height = 600;
+      var margin = {top:20,left:20,right:20,bottom:20};
+      var  xwidth=width-margin.left-margin.right
+      var yheight=height-margin.top-margin.bottom
       var axisLength = width - 2 * margin; 
       data=[
-      	{x:1,input: 400 ,output:200},
-      	{x:2,input: 300 ,output:250},
-      	{x:3,input: 450 ,output:230},
+      	{x:1,input: 100 ,output:200},
+      	{x:2,input: 200 ,output:250},
+      	{x:3,input: 250 ,output:230},
 
       ];	
+      var toggle =true;
       var barwidth= ((width-margin.left)/data.length);
-      console.log(d3);
       var svgContainer = d3.select('svg').
       attr('width',width).
-      attr('height',height)
+      attr('height',height).
+      style('background','black')
       //creating x array
       function getxarray(){
       	var xarray=[];
@@ -26,51 +29,199 @@
       function getyarray(){
       	var yarray=[];
       		data.forEach(function(data){ 
-      			
       			yarray.push(data.input);
-      			yarray.push(data.output);
       		});
       //	console.log(xarray);	
       	return yarray;
       }	
-      
-      
-
-	// creating x array end
 
       //create scales
-      var xscale=d3.scaleLinear().
-      			range([0,width])
-      			.domain(getxarray())
-      			
-      	console.log(xscale.domain());		
-
-      var yscale=d3.scaleBand().
-      				domain(getyarray())
-      							
-
-      			console.log(yscale);					
+      var xmaxrange=d3.max(getxarray());
+      var ymaxrange=d3.max(getyarray());
+      var xscales=d3.scaleLinear()
+      var xscale=	xscales.range([0,xwidth])
+      			.domain([0,xmaxrange])
+      var yscales=d3.scaleLinear()
+      var yscale=yscales.range([0,yheight]).domain([ymaxrange,0]);
 
       //create scales end
       //axes
-      	xaxis=d3.axisBottom(xscale);
-      	yaxis=d3.axisLeft(yscale);
+      	xaxis=d3.axisBottom(xscale).ticks(data.length);
+      	yaxis=d3.axisLeft(yscale).ticks(5);
  						
       console.log(xaxis);
-      var g=svgContainer.append('g')
-      svgContainer.append('g').attr('transform','translate(0,'+(height-margin.bottom)+')').call(xaxis)
-      g.selectAll("rect")
-	   .data(data).
-	   enter().
-	   append('rect').
-	   attr('x',function(d,i){
-	   	return i*(barwidth+margin.right)
-	   }).attr('y',function(d){
-	   	return (height-d.input)
-	   }).
-	   attr('width',barwidth).
-	   attr('height',function(d){
+      var input=svgContainer.append('g')
+      var output=svgContainer.append('g')
+      var path=svgContainer.selectAll('g').selectAll('g')
+      //axis line
+      svgContainer.append('g').attr('transform','translate('+margin.right+','+(height-margin.bottom)+')').attr('class' ,'axisblue').call(xaxis)
+      svgContainer.append('g').attr('transform','translate('+(margin.right)+','+(margin.bottom)+')').attr('class' ,'axisblue').call(yaxis)
+      //axis line end
+      // input data
+      input.selectAll('rect').
+            data(data).enter()
+            .append('rect').
+            attr('x',function(data){
+                 return xscales(data.x)      
+            }).
+            attr('data-index',function(data){
+                  //console.log(yscales(data.input))
+                 return data.x
+            }).
+            attr('y',function(data){
+                  //console.log(yscales(data.input))
+                 return        height-data.input-margin.bottom
+            }).
+            attr('width',function(data){
+                  return 20;
+            }).
+            attr('fill','orange').
+            attr('height',function(data){
+                  return data.input;
+            })
+      //input data end
+      //output data
+       output.selectAll('rect').
+            data(stackeddata(data,height,margin)).enter()
+            .append('rect').
+            attr('x',function(data){
+                 return xscales(data.x)      
+            }).
+            attr('y',function(data){
+                  //console.log(yscales(data.input))
+                 return data.y
+            }).
+             attr('data-index',function(data){
+                  //console.log(yscales(data.input))
+                 return data.x
+            }).
+            attr('width',function(data){
+                  return 20;
+            }).
+            attr('fill','blue').
+            attr('height',function(data){
+                  return data.height;
+            })     
 
-	   	return height-(height-d.input)-margin.bottom;
 
-	   })
+      //output data end 
+
+      //calculate output height function
+            function stackeddata(data,height,margin){
+                  var dataobj=[];
+                  data.forEach(function(datainput,index){
+                        var yvalue=height-(datainput.input+datainput.output)-margin.bottom
+                       dataobj[index]={x:datainput.x,y:yvalue,height:datainput.output} 
+             })
+                  console.log(dataobj);
+                  return dataobj
+            }
+
+           function toggledata(data,height,margin){
+                  if(toggle == true){
+                    toggle=false;          
+                  groupeddata(data,height,margin)
+                  }
+                  else{
+                        toggle=true;          
+                  stackedchadata(data,height,margin)
+                  }
+           }       
+
+          function groupeddata(data,height,margin){
+            var groupedobj=[];
+                  data.forEach(function(datainput,index){
+                        var yvalue=height-datainput.output-margin.bottom;
+                       groupedobj[index]={x:datainput.x,y:yvalue,height:datainput.output} 
+                  });
+                  output.selectAll('rect').data(groupedobj).transition().duration(2000).
+            attr('x',function(data){
+                 return xscales(data.x)-20      
+            }).
+            attr('y',function(data){
+                  //console.log(yscales(data.input))
+                 return data.y
+            }). attr('data-index',function(data){
+                  //console.log(yscales(data.input))
+                 return data.x
+            }).
+            attr('width',function(data){
+                  return 20;
+            }).
+            attr('fill','blue').
+            attr('height',function(data){
+                  return data.height;
+            })     
+
+          } 
+
+
+          function stackedchadata(data,height,margin){
+            var groupedobj=[];
+                  data.forEach(function(datainput,index){
+                        var yvalue=height-(datainput.input+datainput.output)-margin.bottom
+                       groupedobj[index]={x:datainput.x,y:yvalue,height:datainput.output} 
+                  });
+                  output.selectAll('rect').data(groupedobj).transition().duration(2000).
+            attr('x',function(data){
+                 return xscales(data.x)      
+            }).
+            attr('y',function(data){
+                  //console.log(yscales(data.input))
+                 return data.y
+            }). attr('data-index',function(data){
+                  //console.log(yscales(data.input))
+                 return data.x
+            }).
+            attr('width',function(data){
+                  return 20;
+            }).
+            attr('fill','blue').
+            attr('height',function(data){
+                  return data.height;
+            })     
+
+          }  
+
+
+
+      // calculate output function height
+
+
+/*******************circle***********************/
+
+      var svg=d3.select('#circle')       
+                .attr("width", 200)
+                .attr("height", 200)
+                .append("g")
+                .attr("transform", "translate(100,100)");
+
+      
+      jQuery('#root >g >rect').hover(function(){
+          // console.log(jQuery(this).attr('data-index')) 
+           var index=jQuery(this).attr('data-index');
+           console.log(Math.PI*0.25)
+            var arc = d3.arc()
+                .innerRadius(40)
+                .outerRadius(70)
+                .cornerRadius(2)
+
+              
+               var dataarray=[]
+               dataarray.push(data[index-1].input)
+               dataarray.push(data[index-1].output)
+                var pie=d3.pie().value(function(d){
+                  return d;
+                });
+                var color=d3.scaleOrdinal().range(["orange","blue"])
+
+                  var arcs=svg.selectAll('.arcs').exit().remove().data(pie(dataarray)).enter().
+                  append("g").attr("class","arcs")
+                  arcs.append("path").attr('d',arc).attr("fill",function(d){
+                        return color(d.data)
+                  })
+                console.log(pie(dataarray))
+      })       
+              
+
+/*******************circleend***********************/
